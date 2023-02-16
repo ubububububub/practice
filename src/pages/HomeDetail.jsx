ef551f4b8906c496e5eb31c47d4445ea5eb52d4e
ub <kimub1204@naver.com>
2023-02-17 07:25:39 +0900

    react-hook-form study

--- src/pages/HomeDetail.jsx
+++ src/pages/HomeDetail.jsx
@@ -6,13 +6,13 @@ const regex = { email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g };
 
 export default function HomeDetail() {
   const { id } = useParams();
-  const {
-    register,
-    watch,
-    reset,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({ mode: "onSubmit" });
+  // const {
+  //   register,
+  //   watch,
+  //   reset,
+  //   formState: { errors },
+  //   handleSubmit,
+  // } = useForm({ mode: "onSubmit" });
   // const {
   //   register,
   //   reset,
@@ -28,34 +28,16 @@ export default function HomeDetail() {
   //     body: "",
   //   },
   // });
+  const {
+    register,
+    reset,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   useEventItem(reset, id);
 
-  const { id: eventId, userId, title, body } = watch();
-
   const nameRegister = register("name", {
-    required: {
-      value: true,
-      message: "이름은 필수 값입니다.",
-    },
-    maxLength: {
-      value: 5,
-      message: "글자 길이는 최대 5자 입니다.",
-    },
-  });
-  const ageRegister = register("age");
-  const sexRegister = register("sex");
-  const priceRegister = register("price", {
-    required: {
-      value: true,
-      message: "가격은 필수 값입니다.",
-    },
-    min: {
-      value: 300,
-      message: "최소 가격은 300원 입니다.",
-    },
-  });
-  const foodRegister = register("food", {
-    required: { value: true, message: "음식은 필수 값 입니다." },
+    required: { value: true, message: "이름은 필수 값입니다." },
   });
 
   const onSubmit = (data) => console.log(data);
@@ -65,15 +47,19 @@ export default function HomeDetail() {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit, onError)}>
-        {/* <input value={eventId} />
-        <input value={userId} />
-        <input value={title} />
-        <input value={body} /> */}
-        <input placeholder="name" {...nameRegister} />
-        <input placeholder="age" {...ageRegister} />
-        <input placeholder="sex" {...sexRegister} />
-        <input type="number" placeholder="0" {...priceRegister} />
-        <label>
+        <div>
+          <label htmlFor="name">이름</label>
+          <input
+            id="name"
+            placeholder="이름을 입력해주세요."
+            {...nameRegister}
+          />
+        </div>
+        {errors.name && <div>{errors.name.message}</div>}
+        {/* <input placeholder="age" {...ageRegister} /> */}
+        {/* <input placeholder="sex" {...sexRegister} /> */}
+        {/* <input type="number" placeholder="0" {...priceRegister} /> */}
+        {/* <label>
           <input name="food" type="radio" {...foodRegister} value="피자" />
           피자
         </label>
@@ -84,13 +70,9 @@ export default function HomeDetail() {
         <label>
           <input name="food" type="radio" {...foodRegister} value="치킨" />
           치킨
-        </label>
+        </label> */}
         <button type="submit">버튼</button>
       </form>
-
-      {errors && <div>{errors?.name?.message}</div>}
-      {errors && <div>{errors?.price?.message}</div>}
-      {errors && <div>{errors?.food?.message}</div>}
     </div>
   );
 }
